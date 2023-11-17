ca1d6718966a26efecc8aa96943ec3fcdee5e651
Steven <stevenwu4444@gmail.com>
2023-11-17 15:25:25 -0500

    first commit

--- api/videoGames.js
+++ api/videoGames.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-const REPLACE_ME = 'HELP REPLACE ME!!!!';
-
 const { getAllVideoGames,
     getVideoGameById,
     createVideoGame,
